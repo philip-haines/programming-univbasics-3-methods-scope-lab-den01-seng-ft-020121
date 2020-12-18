b1116a279578bcebdf2096daef27c370f4dd89bf
eager-monitor-013e <phil.haines92@gmail.com>
2020-12-18 21:14:39 +0000

    Automatically backed up by Learn

--- lib/catch_phrases.rb
+++ lib/catch_phrases.rb
@@ -1,5 +1,5 @@
 def mario
-  phrase = "It's-a me, Mario"
+  phrase = "It's-a me, Mario!"
   puts phrase
 end
 
@@ -9,7 +9,7 @@ def toadstool
 end
 
 def link
-  phrase = "It's Dangerous To Go Alone! Take This"
+  phrase = "It's Dangerous To Go Alone! Take This."
   puts phrase
 end
 
