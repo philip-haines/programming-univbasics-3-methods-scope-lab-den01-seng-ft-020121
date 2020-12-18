2f7788662725e182a73f83482422f04bff8200dc
eager-monitor-013e <phil.haines92@gmail.com>
2020-12-18 21:10:42 +0000

    Automatically backed up by Learn

--- lib/catch_phrases.rb
+++ lib/catch_phrases.rb
@@ -4,6 +4,13 @@ def mario
 end
 
 def toadstool
-   status = 'Thank You Mario! But Our Princess Is In Another Castle!'
+  status = 'Thank You Mario! But Our Princess Is In Another Castle!'
   puts status
 end
+
+def link
+  phrase = "It's Dangerous To Go Alone! Take This"
+end
+
+def all_phrases
+  
